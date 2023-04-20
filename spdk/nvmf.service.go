ad2713771c2a923841cd8ab3c6e265a4adc2b2a3
Boris Glimcher <36732377+glimchb@users.noreply.github.com>
2023-04-20 10:23:37 -0400

    Create nvmf.service.go
    Signed-off-by: Boris Glimcher <36732377+glimchb@users.noreply.github.com>

---
+++ spdk/nvmf.service.go
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+
+// Package spdk implements the spdk json-rpc protocol
+package spdk
+
+type NvmfService interface {
+	CreateSubsystem(*NvmfCreateSubsystemParams) (*NvmfCreateSubsystemResult, error)
+	DeleteSubsystem(*NvmfDeleteSubsystemParams) (*NvmfDeleteSubsystemResult, error)
+	GetSubsystems(page int, limit int) (*NvmfGetSubsystemsResult, error)
+	GetStats(page int, limit int) (*NvmfGetSubsystemStatsResult, error)
+ 	AddListener(*NvmfSubsystemAddListenerParams) (*NvmfSubsystemAddListenerResult, error)
+ 	RemoveListener(*NvmfSubsystemAddListenerParams) (*NvmfSubsystemAddListenerResult, error)
+ 	AddNamespace(*NvmfSubsystemAddNsParams) (*NvmfSubsystemAddNsResult, error)
+ 	RemoveNamespace(*NvmfSubsystemRemoveNsParams) (*NvmfSubsystemRemoveNsResult, error)
+
+}
